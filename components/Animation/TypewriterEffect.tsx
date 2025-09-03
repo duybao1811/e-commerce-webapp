cfcf1160b782ea6d04daa068a31806fde9b71545
DuyBaoAegona <bao.luongduy@pizza4ps.com>
2025-09-03 17:03:47 +0700

    feat: add animation countdown

---
+++ components/Animation/TypewriterEffect.tsx
@@ -0,0 +1,53 @@
+'use client'
+import { motion } from "framer-motion";
+
+const container = {
+  hidden: { opacity: 0 },
+  visible: (i = 1) => ({
+    opacity: 1,
+    transition: { staggerChildren: 0.05, delayChildren: 0.5 },
+  }),
+};
+
+const child = {
+  hidden: {
+    opacity: 0,
+    y: 20,
+    transition: {
+      type: "spring",
+      damping: 12,
+      stiffness: 100,
+    },
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      type: "spring",
+      damping: 12,
+      stiffness: 100,
+    },
+  },
+};
+
+interface Props {
+  text: string;
+}
+
+function TypewriterEffect({ text }: Props) {
+  return (
+    <motion.div
+      variants={container}
+      initial="hidden"
+      animate="visible"
+    >
+      {text.split("").map((char, index) => (
+        <motion.span key={index} variants={child}>
+          {char === " " ? "\u00A0" : char}
+        </motion.span>
+      ))}
+    </motion.div>
+  );
+}
+
+export default TypewriterEffect;
---
+++ lib/hooks/useCountdownTimer.tsx
@@ -0,0 +1,66 @@
+'use client'
+import { useState, useEffect } from 'react';
+
+interface IProps {
+  day?: number;
+  hour?: number;
+  minute?: number;
+  second?: number;
+}
+
+interface ICountdown {
+  days: number;
+  hours: number;
+  minutes: number;
+  seconds: number;
+}
+
+const useCountdownTimer = ({
+ day = 0,
+ hour = 0,
+ minute = 0,
+ second = 0,
+}: IProps) => {
+  const now = new Date();
+
+  const targetDate = new Date(
+    now.getFullYear(),
+    now.getMonth(),
+    now.getDate() + day,
+    now.getHours() + hour,
+    now.getMinutes() + minute,
+    now.getSeconds() + second,
+  ).getTime();
+
+  const [countDown, setCountDown] = useState<ICountdown>({
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  });
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const currentTime = new Date().getTime();
+      const distance = targetDate - currentTime;
+
+      if (distance < 0) {
+        clearInterval(interval);
+        setCountDown({ days: 0, hours: 0, minutes: 0, seconds: 0 });
+      } else {
+        const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        setCountDown({ days, hours, minutes, seconds });
+      }
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [day, hour, minute, second]);
+
+  return countDown;
+};
+
+export default useCountdownTimer;
