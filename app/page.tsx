da078b9bd40c5a7f50d39b1b8f6dd4f723992f2a
DuyBaoAegona <bao.luongduy@pizza4ps.com>
2025-09-04 10:27:01 +0700

    feat: custom animation countdown

--- app/page.tsx
+++ app/page.tsx
@@ -9,12 +9,8 @@ import PromoCountdownBanner from "@/views/Home/PromoCountdownBanner";
 
 const HomePage = () => {
   return (
-<<<<<<< HEAD
     <div className={"flex flex-col gap-[70px] container-custom pt-20 pb-30"}>
-=======
-    <div className={"flex flex-col gap-[70px] px-32 pt-20 pb-30"}>
       <FlashSales />
->>>>>>> 281a7bf (feat: add flash sale section)
       <Categories />
       <div className={"divider"} />
       <BestSelling />
